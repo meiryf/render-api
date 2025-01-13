7e34eeefe1a3c52e11c2a0efff9853d26996bc95
meiryf <meirfriedman9@gmail.com>
2025-01-12 22:42:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,17 @@
 from flask import Flask, request, jsonify
-from selenium import webdriver
-import os
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, this is your API home!"
 
 @app.route('/scrape', methods=['POST'])
 def scrape():
     data = request.get_json()
     url = data.get('url')
-
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument('--no-sandbox')
-
-    driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), options=chrome_options)
-    driver.get(url)
-    page_content = driver.page_source
-    driver.quit()
-
-    return jsonify({'content': page_content})
+    # Add your scraping logic here
+    return jsonify({"success": True, "message": f"Successfully scraped data from {url}", "data": {}})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
